a5b3550b0f5a2680018ee4b143d3f448524eea82
mySpark <zhibo.sheng@authright.com>
2020-09-17 11:06:00 -0700

    add file structure

--- src/main/java/com/authright/timesheet/TimesheetApplication.java
+++ src/main/java/com/authright/timesheet/TimesheetApplication.java
@@ -2,8 +2,10 @@ package com.authright.timesheet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan(basePackages = {"timesheet"})
 public class TimesheetApplication {
 
     public static void main(String[] args) {
